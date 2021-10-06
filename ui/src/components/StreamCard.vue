8a1c3df4f9c0c682fbf62c55a0a23414ce3549b2
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-10-06 11:39:02 +0100

    feat(ui): simplify and add receive / open btns

--- ui/src/components/StreamCard.vue
+++ ui/src/components/StreamCard.vue
@@ -1,42 +1,72 @@
 <template>
-  <v-card color="" class="mt-5 mb-5" style="transition: all 0.2s ease-in-out">
-    <v-row>
-      <v-col>
-        <v-toolbar class="transparent elevation-0" dense>
-          <v-toolbar-title>{{ stream.name }}</v-toolbar-title>
-          <v-spacer />
-        </v-toolbar>
-        <v-card-text class="transparent elevation-0 mt-0 pt-0" dense>
-          <v-toolbar-title>
-            <v-chip v-if="stream.role" small class="mr-1">
-              <v-icon small left>mdi-account-key-outline</v-icon>
-              {{ stream.role.split(':')[1] }}
-            </v-chip>
-            <v-chip small class="mr-1">
-              Updated
-              <timeago :datetime="stream.updatedAt" class="ml-1"></timeago>
-            </v-chip>
-            <v-chip v-if="stream.branches" small>
-              <v-icon small class="mr-2 float-left">mdi-source-branch</v-icon>
-              {{ stream.branches.totalCount }}
-            </v-chip>
-          </v-toolbar-title>
-        </v-card-text>
-      </v-col>
-      <v-col md="auto" />
-      <v-col align="end" justify="center">
-        <v-btn fab :loading="loading" class="mr-4 elevation-1 btn-fix" hint="Send" @click="send">
-          <v-img
-            v-if="$vuetify.theme.dark"
-            src="@/assets/SenderWhite.png"
-            max-width="40"
-            style="display: inline-block"
-          />
-          <v-img v-else src="@/assets/Sender.png" max-width="40" style="display: inline-block" />
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-card color="" class="mt-5 mb-5" style="transition: all 0.2s ease-in-out">
+      <v-row>
+        <v-col>
+          <v-toolbar class="transparent elevation-0" dense>
+            <v-toolbar-title>{{ stream.name }}</v-toolbar-title>
+            <v-spacer />
+          </v-toolbar>
+          <v-card-text class="transparent elevation-0 mt-0 pt-0" dense>
+            <v-toolbar-title>
+              <v-chip v-if="stream.role" small class="mr-1">
+                <v-icon small left>mdi-account-key-outline</v-icon>
+                {{ stream.role.split(':')[1] }}
+              </v-chip>
+              <v-chip small class="mr-1">
+                Updated
+                <timeago :datetime="stream.updatedAt" class="ml-1"></timeago>
+              </v-chip>
+              <v-chip v-if="stream.branches" small>
+                <v-icon small class="mr-2 float-left">mdi-source-branch</v-icon>
+                {{ stream.branches.totalCount }}
+              </v-chip>
+            </v-toolbar-title>
+          </v-card-text>
+        </v-col>
+        <v-col v-if="hover" align="end" justify="center">
+          <v-btn v-tooltip="'Open in browser'" icon class="mr-4 btn-fix" @click="openInWeb">
+            <v-icon>mdi-open-in-new</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            :loading="loadingSend"
+            class="mr-4 elevation-1 btn-fix"
+            hint="Send"
+            @click="send"
+          >
+            <v-img
+              v-if="$vuetify.theme.dark"
+              src="@/assets/SenderWhite.png"
+              max-width="40"
+              style="display: inline-block"
+            />
+            <v-img v-else src="@/assets/Sender.png" max-width="40" style="display: inline-block" />
+          </v-btn>
+          <v-btn
+            fab
+            :loading="loadingReceive"
+            class="mr-4 elevation-1 btn-fix"
+            hint="Receive"
+            @click="receive"
+          >
+            <v-img
+              v-if="$vuetify.theme.dark"
+              src="@/assets/ReceiverWhite.png"
+              max-width="40"
+              style="display: inline-block"
+            />
+            <v-img
+              v-else
+              src="@/assets/Receiver.png"
+              max-width="40"
+              style="display: inline-block"
+            />
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
@@ -59,7 +89,7 @@ export default {
     }
   },
   data() {
-    return { loading: false }
+    return { loadingSend: false, loadingReceive: false }
   },
   computed: {},
   mounted() {
@@ -74,15 +104,18 @@ export default {
     sleep(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms))
     },
+    openInWeb() {
+      window.open(`${localStorage.getItem('serverUrl')}/streams/${this.stream.id}`)
+    },
     async send() {
-      this.loading = true
+      this.loadingSend = true
       sketchup.send_selection(this.stream.id)
       console.log('request for data sent to sketchup')
       await this.sleep(2000)
     },
     async createCommit(objects) {
       if (objects.length == 0) {
-        this.loading = false
+        this.loadingSend = false
         return
       }
 
@@ -91,7 +124,7 @@ export default {
 
       console.log('objects:', s.objects)
       try {
-        this.loading = true
+        this.loadingSend = true
         let batches = s.batchObjects()
         for (const batch of batches) {
           let res = await this.sendBatch(batch)
@@ -120,9 +153,9 @@ export default {
         })
         console.log('sent to stream: ' + this.stream.id)
 
-        this.loading = false
+        this.loadingSend = false
       } catch (err) {
-        this.loading = false
+        this.loadingSend = false
         console.log(err)
       }
     },
--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -1,25 +1,5 @@
 <template>
-  <v-container>
-    <streams-list />
-
-    <!-- testing sketchup bindings -->
-    <v-container>
-      <v-card>
-        <v-card-title>Testing Sketchup Bindings</v-card-title>
-        <v-card-text>
-          <div class="d-flex align-center">
-            Name:
-            <v-text-field v-model="name" hint="Name" class="ml-3" />
-          </div>
-          <div class="d-flex align-center">
-            Pokes:
-            <v-text-field v-model.number="num_pokes" class="ml-3" />
-          </div>
-          <v-btn @click="poke">Poke {{ name }}!</v-btn>
-        </v-card-text>
-      </v-card>
-    </v-container>
-  </v-container>
+  <v-container></v-container>
 </template>
 
 <script>
@@ -32,15 +12,10 @@ global.clickFromSettings = function (args) {
 }
 
 export default {
-  name: 'Streams',
-  components: {
-    StreamsList: () => import('@/components/StreamsList')
-  },
+  name: 'Settings',
+  components: {},
   data() {
-    return {
-      name: 'Dim',
-      num_pokes: 3
-    }
+    return {}
   },
   computed: {},
   apollo: {
@@ -52,10 +27,6 @@ export default {
     bus.$on('click-from-main', (args) => console.log('triggered from main', args))
     bus.$on('click-from-settings', (args) => console.log('triggered from settings', args))
   },
-  methods: {
-    poke() {
-      sketchup.poke(this.name, this.num_pokes)
-    }
-  }
+  methods: {}
 }
 </script>
