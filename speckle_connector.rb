171bf4cc46734d36410f27d5df7b09031a0a37dc
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-10-07 18:20:52 +0100

    feat(connector): generalise connector path

--- speckle_connector.rb
+++ speckle_connector.rb
@@ -4,26 +4,21 @@ require "extensions"
 
 module SpeckleSystems
   module SpeckleConnector
-
     file = __FILE__.dup
     # Account for Ruby encoding bug under Windows.
-    file.force_encoding('UTF-8') if file.respond_to?(:force_encoding)
+    file.force_encoding("UTF-8") if file.respond_to?(:force_encoding)
     # Support folder should be named the same as the root .rb file.
-    folder_name = ::File.basename(file, '.*')
+    folder_name = File.basename(file, ".*")
 
     # Path to the root .rb file (this file).
-    PATH_ROOT = ::File.dirname(file).freeze
+    PATH_ROOT = File.dirname(file).freeze
 
     # Path to the support folder.
-    PATH = ::File.join(PATH_ROOT, folder_name).freeze
+    PATH = File.join(PATH_ROOT, folder_name).freeze
 
     unless file_loaded?(__FILE__)
 
-      ex = SketchupExtension.new(
-        "Hello Cube",
-        "C:/Users/admin/Code/speckle-sketchup/speckle_connector/main"
-        # "C:/Users/izzy lyseggen/Documents/dev/next/sketchup_connector/speckle_connector/main"
-      )
+      ex = SketchupExtension.new("Speckle SketchUp", File.join(PATH, "main"))
 
       ex.description = "Speckle Connector for SketchUp"
 
