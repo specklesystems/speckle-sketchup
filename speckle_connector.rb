bfd71ba31c7adeac6ef95a38c3f2a5f357455020
izzy lyseggen <izzy.lyseggen@gmail.com>
2022-03-07 11:43:11 +0000

    fix: turn off dev

--- speckle_connector.rb
+++ speckle_connector.rb
@@ -20,7 +20,7 @@ module SpeckleSystems
     PATH = File.join(PATH_ROOT, folder_name).freeze
 
     # Run from localhost or from build files
-    DEV_MODE = true
+    DEV_MODE = false
     puts("Loading Speckle Connector v#{CONNECTOR_VERSION} from #{DEV_MODE ? 'dev' : 'build'}")
 
 
