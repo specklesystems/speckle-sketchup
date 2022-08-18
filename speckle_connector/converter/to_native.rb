9ca55f6f0ed31d943fc972b458476c8a850afe5e
oguzhankoral <45078678+oguzhankoral@users.noreply.github.com>
2022-08-18 16:42:16 +0300

    Merge coplanar faces (#58)
    * Update gitignore for RubyMine IDE
    
    * Update gemfiles
    
    - Include skippy, pry, rubycritic
    
    * Add speckle_connector loader
    
    This helps to navigate files that read by SU according to packaged version or development version
    
    * Update gemfile and .lock files
    
    * Merge coplanar faces by erasing edges between
    
    * Cite about the idea behind merging coplanar faces
    
    * Document remove_edge_have_coplanar_faces with control steps
    
    * Improve loader file with the idea of release version
    
    - This is another issue that need to be navigated
    
    * Remove speckle_connector_loader

--- speckle_connector/converter/to_native.rb
+++ speckle_connector/converter/to_native.rb
@@ -119,8 +119,126 @@ module SpeckleSystems::SpeckleConnector::ToNative
       native_mesh.add_polygon(indices.map { |index| points[index] })
     end
     entities.add_faces_from_mesh(native_mesh, 4, material_to_native(mesh["renderMaterial"]))
+    merge_coplanar_faces(entities)
     native_mesh
   end
+
+  # Removes coplanar entities from the given entities.
+  # @param entities [Sketchup::Entities] entities to remove edges between that make entities coplanar.
+  # @note Merging coplanar faces idea originated from [CleanUp](https://github.com/thomthom/cleanup) plugin
+  # which is developed by [Thomas Thomassen](https://github.com/thomthom).
+  def merge_coplanar_faces(entities)
+    edges = []
+    faces = entities.collect { |entity| entity if entity.is_a? Sketchup::Face }.compact
+    faces.each { |face| face.edges.each { |edge| edges << edge } }
+    edges.compact!
+    edges.each { |edge| remove_edge_have_coplanar_faces(edge, false ) }
+  end
+
+  # Detect edges to remove by checking following controls respectively;
+  #  - Upcoming Sketchup entity is Sketchup::Edge or not.
+  #  - Whether edge has 2 face or not.
+  #  - Whether faces are duplicated or not.
+  #  - Whether edges safe to merge or not.
+  #  - Whether faces have same material or not.
+  #  - Whether UV texture map is aligned between faces or not.
+  #  - Finally, if faces are coplanar by correcting these checks, then removes edge from Sketchup.active_model.
+  # @param edge [Sketchup::Edge] edge to check.
+  # @param ignore_materials [Boolean] whether ignore materials or not.
+  # Returns true if the given edge separating two coplanar faces.
+  # Return false otherwise.
+  def remove_edge_have_coplanar_faces(edge, ignore_materials)
+    return false unless edge.valid? && edge.is_a?(Sketchup::Edge)
+    return false unless edge.faces.size == 2
+
+    face1, face2 = edge.faces
+
+    return false if face_duplicate?(face1, face2)
+    # Check for troublesome faces which might lead to missing geometry if merged.
+    return false unless edge_safe_to_merge?(edge)
+
+    # Check materials match.
+    unless ignore_materials
+      if face1.material == face2.material && face1.back_material == face2.back_material
+        # Verify UV mapping match.
+        unless face1.material.nil? || face1.material.texture.nil?
+          return false unless continuous_uv?(face1, face2, edge)
+        end
+      else
+        return false
+      end
+    end
+    # Check faces are coplanar or not.
+    return false unless faces_coplanar?(face1, face2)
+    edge.erase!
+    true
+  end
+
+  # Determines if two faces are overlapped.
+  def face_duplicate?(face1, face2, overlapping = false)
+    return false if face1 == face2
+
+    v1 = face1.outer_loop.vertices
+    v2 = face2.outer_loop.vertices
+    return true if (v1 - v2).empty? && (v2 - v1).empty?
+
+    if overlapping && (v2 - v1).empty?
+      edges = (face2.outer_loop.edges - face1.outer_loop.edges)
+      unless edges.empty?
+        point = edges[0].start.position.offset(edges[0].line[1], 0.01)
+        return true if face1.classify_point(point) <= 4
+      end
+    end
+    false
+  end
+
+  # Checks the given edge for potential problems if the connected faces would
+  # be merged.
+  def edge_safe_to_merge?(edge)
+    edge.faces.all? { |face| self.face_safe_to_merge?(face) }
+  end
+
+  # Returns true if the two faces connected by the edge has continuous UV mapping.
+  # UV's are normalized to 0.0..1.0 before comparison.
+  def continuous_uv?(face1, face2, edge)
+    tw = Sketchup.create_texture_writer
+    uvh1 = face1.get_UVHelper(true, true, tw)
+    uvh2 = face2.get_UVHelper(true, true, tw)
+    p1 = edge.start.position
+    p2 = edge.end.position
+    self.uv_equal?(uvh1.get_front_UVQ(p1), uvh2.get_front_UVQ(p1)) &&
+      self.uv_equal?(uvh1.get_front_UVQ(p2), uvh2.get_front_UVQ(p2)) &&
+      self.uv_equal?(uvh1.get_back_UVQ(p1), uvh2.get_back_UVQ(p1)) &&
+      self.uv_equal?(uvh1.get_back_UVQ(p2), uvh2.get_back_UVQ(p2))
+  end
+
+  # Normalize UV's to 0.0..1.0 and compare them.
+  def uv_equal?(uvq1, uvq2)
+    uv1 = uvq1.to_a.map { |n| n % 1 }
+    uv2 = uvq2.to_a.map { |n| n % 1 }
+    uv1 == uv2
+  end
+
+  # Validates that the given face can be merged with other faces without causing
+  # problems.
+  def face_safe_to_merge?(face)
+    stack = face.outer_loop.edges
+    edge = stack.shift
+    direction = edge.line[1]
+    until stack.empty?
+      edge = stack.shift
+      return true unless edge.line[1].parallel?(direction)
+    end
+    false
+  end
+
+  # Determines if two faces are coplanar.
+  def faces_coplanar?(face1, face2)
+    vertices = face1.vertices + face2.vertices
+    plane = Geom.fit_plane_to_points(vertices)
+    vertices.all? { |v| v.position.on_plane?(plane) }
+  end
+
 
   def _hidden_edges_mesh_to_native_mesh(mesh, entities)
     native_mesh = Geom::PolygonMesh.new(mesh["vertices"].count / 3)
