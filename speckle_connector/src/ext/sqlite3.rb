4df1cc17bf2ce03864c45d61cdf07def8b91b71c
oguzhankoral <oguzhankoral@gmail.com>
2022-11-24 03:04:09 +0300

    Add switch for operating system

--- speckle_connector/src/ext/sqlite3.rb
+++ speckle_connector/src/ext/sqlite3.rb
@@ -1,15 +1,13 @@
-# support multiple ruby version (fat binaries under windows)
-begin
-  RUBY_VERSION =~ /(\d+\.\d+)/
-  require "sqlite3/#{$1}/sqlite3_native"
-rescue LoadError
-  require 'sqlite3/sqlite3_native'
-end
+# frozen_string_literal: true
 
-require_relative 'sqlite3/database'
-require_relative 'sqlite3/version'
+require_relative '../constants/platform_constants'
 
-module SQLite3
-  # Was sqlite3 compiled with thread safety on?
-  def self.threadsafe?; threadsafe > 0; end
+module SpeckleConnector
+  extension = if OPERATING_SYSTEM == OS_WIN
+                'so'
+              else
+                'bundle'
+              end
+  sqlite3_file = "sqlite3_#{RUBY_VERSION_NUMBER}.#{extension}"
+  require_relative(File.join('sqlite3', sqlite3_file))
 end
