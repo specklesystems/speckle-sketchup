ae6efcc27da92e47d714ccff82239fb7d0ba413f
oguzhankoral <oguzhankoral@gmail.com>
2022-12-12 12:51:45 +0300

    Add sketchup unit value to sketchup state

--- speckle_connector/src/states/sketchup_state.rb
+++ speckle_connector/src/states/sketchup_state.rb
@@ -7,13 +7,18 @@ module SpeckleConnector
     # Sketchup model state holds information about sketchup related objects like model, layers, materials etc.
     class SketchupState
       include Immutable::ImmutableUtils
-
       # @return [Sketchup::Model] active model on the sketchup
       attr_reader :sketchup_model
 
       # @param sketchup_model [Sketchup::Model] active model on the sketchup
       def initialize(sketchup_model)
         @sketchup_model = sketchup_model
+      end
+
+      # @return [Integer] length units code of the sketchup model.
+      #  @example { 0 => 'in', 1 => 'ft', 2 => 'mm', 3 => 'cm', 4 => 'm', 5 => 'yd' }
+      def length_units
+        sketchup_model.options['UnitsOptions']['LengthUnit']
       end
     end
   end
