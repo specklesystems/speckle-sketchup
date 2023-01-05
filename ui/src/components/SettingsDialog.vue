c63c0675d5fba055f3318f21bf2a92d4e3f40d91
oguzhankoral <oguzhankoral@gmail.com>
2023-01-05 12:39:08 +0300

    Init settings dialog

---
+++ ui/src/components/SettingsDialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- DIALOG: Settings -->
+  <v-dialog v-model="showSettings">
+    <template #activator="{ on, attrs }">
+      <v-btn
+          icon small class="mx-1"
+          v-bind="attrs"
+          v-on="on"
+      >
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5">
+        Settings
+      </v-card-title>
+      <v-container class="px-6" pb-0>
+        <!-- Switch Theme -->
+        <v-btn icon small class="mx-1" @click="switchTheme">
+          <v-icon>mdi-theme-light-dark</v-icon>
+        </v-btn>
+        <span>Color Mode</span>
+
+      </v-container>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="blue darken-1"
+            text
+            @click="showSettings = false"
+        >
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import {bus} from "@/main";
+
+export default {
+  name: "ShowSettings",
+  data() {
+    return {
+      showSettings: false,
+      streamName: "",
+      description: "",
+    }
+  },
+  watch: {
+    'showSettings': {
+      handler(newValue, oldValue) {
+        if (newValue){
+          console.log("mix panel triggered when dialog opened")
+          this.$mixpanel.track('Connector Action', { name: 'Open Settings Dialog' })
+        }
+      },
+      deep: true
+    }
+  },
+  methods: {
+    switchTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('theme', this.$vuetify.theme.dark ? 'dark' : 'light')
+      this.$mixpanel.track('Connector Action', { name: 'Toggle Theme' })
+    },
+    refresh() {
+      this.$apollo.queries.user.refetch()
+      bus.$emit('refresh-streams')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
