8cd9673eec877060ee6028e88947e907bdc48247
oguzhankoral <oguzhankoral@gmail.com>
2023-01-19 20:05:44 +0300

    Send scenes to speckle

---
+++ speckle_connector/src/speckle_objects/built_elements/view3d.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative '../base'
+require_relative '../../speckle_objects/geometry/point'
+require_relative '../../speckle_objects/geometry/vector'
+
+module SpeckleConnector
+  module SpeckleObjects
+    module BuiltElements
+      # View3d object represents scenes on Sketchup.
+      class View3d < Base
+        SPECKLE_TYPE = 'Objects.BuiltElements.View:Objects.BuiltElements.View3D'
+
+        # @param name [String] name of the scene
+        # @param origin [SpeckleObjects::Geometry::Point] origin (eye) of the view.
+        # @param target [SpeckleObjects::Geometry::Point] target of the view.
+        # @param direction [SpeckleObjects::Geometry::Vector] direction of the view from eye to target.
+        # @param up_direction [SpeckleObjects::Geometry::Vector] up direction of the view.
+        # @param is_perspective [Boolean] whether view is perspective or not.
+        # @param lens [Boolean] fov value of the view camera.
+        # @param units [String] units of the camera.
+        # @param application_id [String] application_id of the view.
+        # rubocop:disable Metrics/ParameterLists
+        def initialize(name, origin, target, direction, up_direction,
+                       is_perspective, lens, units, application_id)
+          super(
+            speckle_type: SPECKLE_TYPE,
+            total_children_count: 0,
+            application_id: application_id,
+            id: nil
+          )
+          self[:name] = name
+          self[:origin] = origin
+          self[:target] = target
+          self[:forwardDirection] = direction
+          self[:upDirection] = up_direction
+          self[:isOrthogonal] = !is_perspective
+          self[:lens] = lens
+          self[:units] = units
+        end
+        # rubocop:enable Metrics/ParameterLists
+      end
+    end
+  end
+end
