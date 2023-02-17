bb7590eab476edeb2dd5c8c5f57fa10eba3f75c1
oguzhankoral <oguzhankoral@gmail.com>
2023-02-17 22:22:26 +0300

    Simplify sending rendering_options

--- speckle_connector/src/speckle_objects/other/rendering_options.rb
+++ speckle_connector/src/speckle_objects/other/rendering_options.rb
@@ -8,77 +8,16 @@ module SpeckleConnector
       # Rendering options for scenes.
       class RenderingOptions
         # @param options [Sketchup::RenderingOptions] rendering options to convert speckle object
-        # rubocop:disable Metrics/AbcSize
-        # rubocop:disable Metrics/MethodLength
         def self.to_speckle(options)
-          {
-            BackgroundColor: Color.to_speckle(options['BackgroundColor']),
-            BandColor: Color.to_speckle(options['BandColor']),
-            ConstructionColor: Color.to_speckle(options['ConstructionColor']),
-            DepthQueWidth: options['DepthQueWidth'],
-            DisplayColorByLayer: options['DisplayColorByLayer'],
-            DisplayDims: options['DisplayDims'],
-            DisplayFog: options['DisplayFog'],
-            DisplayInstanceAxes: options['DisplayInstanceAxes'],
-            DisplaySectionCuts: options['DisplaySectionCuts'],
-            DisplaySectionPlanes: options['DisplaySectionPlanes'],
-            DisplaySketchAxes: options['DisplaySketchAxes'],
-            DisplayText: options['DisplayText'],
-            DisplayWatermarks: options['DisplayWatermarks'],
-            DrawBackEdges: options['DrawBackEdges'],
-            DrawDepthQue: options['DrawDepthQue'],
-            DrawGround: options['DrawGround'],
-            DrawHidden: options['DrawHidden'],
-            DrawHiddenGeometry: options['DrawHiddenGeometry'],
-            DrawHiddenObjects: options['DrawHiddenObjects'],
-            DrawHorizon: options['DrawHorizon'],
-            DrawLineEnds: options['DrawLineEnds'],
-            DrawProfilesOnly: options['DrawProfilesOnly'],
-            DrawSilhouettes: options['DrawSilhouettes'],
-            DrawUnderground: options['DrawUnderground'],
-            EdgeColorMode: options['EdgeColorMode'],
-            EdgeDisplayMode: options['EdgeDisplayMode'],
-            EdgeType: options['EdgeType'],
-            ExtendLines: options['ExtendLines'],
-            FaceBackColor: Color.to_speckle(options['FaceBackColor']),
-            FaceFrontColor: Color.to_speckle(options['FaceFrontColor']),
-            FogColor: Color.to_speckle(options['FogColor']),
-            FogEndDist: options['FogEndDist'],
-            FogStartDist: options['FogStartDist'],
-            FogUseBkColor: options['FogUseBkColor'],
-            ForegroundColor: Color.to_speckle(options['ForegroundColor']),
-            GroundColor: Color.to_speckle(options['GroundColor']),
-            GroundTransparency: options['GroundTransparency'],
-            HideConstructionGeometry: options['HideConstructionGeometry'],
-            HighlightColor: Color.to_speckle(options['HighlightColor']),
-            HorizonColor: Color.to_speckle(options['HorizonColor']),
-            InactiveFade: options['InactiveFade'],
-            InactiveHidden: options['InactiveHidden'],
-            InstanceFade: options['InstanceFade'],
-            InstanceHidden: options['InstanceHidden'],
-            JitterEdges: options['JitterEdges'],
-            LineEndWidth: options['LineEndWidth'],
-            LineExtension: options['LineExtension'],
-            LockedColor: Color.to_speckle(options['LockedColor']),
-            MaterialTransparency: options['MaterialTransparency'],
-            ModelTransparency: options['ModelTransparency'],
-            RenderMode: options['RenderMode'],
-            SectionActiveColor: Color.to_speckle(options['SectionActiveColor']),
-            SectionCutDrawEdges: options['SectionCutDrawEdges'],
-            SectionCutFilled: options['SectionCutFilled'],
-            SectionCutWidth: options['SectionCutWidth'],
-            SectionDefaultCutColor: Color.to_speckle(options['SectionDefaultCutColor']),
-            SectionDefaultFillColor: Color.to_speckle(options['SectionDefaultFillColor']),
-            SectionInactiveColor: Color.to_speckle(options['SectionInactiveColor']),
-            ShowViewName: options['ShowViewName'],
-            SilhouetteWidth: options['SilhouetteWidth'],
-            SkyColor: Color.to_speckle(options['SkyColor']),
-            Texture: options['Texture'],
-            TransparencySort: options['TransparencySort']
-          }
+          options.to_h.collect do |option_prop, option_value|
+            speckle_value = if option_value.is_a?(Sketchup::Color)
+                              Color.to_speckle(option_value)
+                            else
+                              option_value
+                            end
+            [option_prop.to_sym, speckle_value]
+          end.to_h
         end
-        # rubocop:enable Metrics/AbcSize
-        # rubocop:enable Metrics/MethodLength
       end
     end
   end
