8f3a8bfa914c6478eaa7bd0b5719012f973c0209
oguzhankoral <oguzhankoral@gmail.com>
2023-02-22 12:53:26 +0300

    Validate user and model preferences while reading

---
+++ speckle_connector/src/constants/pref_constants.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module SpeckleConnector
+  COMBINE_FACES_BY_MATERIAL = :combine_faces_by_material
+  INCLUDE_ENTITY_ATTRIBUTES = :include_entity_attributes
+  INCLUDE_FACE_ENTITY_ATTRIBUTES = :include_face_entity_attributes
+  INCLUDE_EDGE_ENTITY_ATTRIBUTES = :include_edge_entity_attributes
+  INCLUDE_GROUP_ENTITY_ATTRIBUTES = :include_group_entity_attributes
+  INCLUDE_COMPONENT_ENTITY_ATTRIBUTES = :include_component_entity_attributes
+  MERGE_COPLANAR_FACES = :merge_coplanar_faces
+
+  DEFAULT_MODEL_PREFERENCES = {
+    COMBINE_FACES_BY_MATERIAL => true,
+    INCLUDE_ENTITY_ATTRIBUTES => true,
+    INCLUDE_FACE_ENTITY_ATTRIBUTES => true,
+    INCLUDE_EDGE_ENTITY_ATTRIBUTES => true,
+    INCLUDE_GROUP_ENTITY_ATTRIBUTES => true,
+    INCLUDE_COMPONENT_ENTITY_ATTRIBUTES => true,
+    MERGE_COPLANAR_FACES => true
+  }.freeze
+end
--- speckle_connector/src/preferences/preferences.rb
+++ speckle_connector/src/preferences/preferences.rb
@@ -3,11 +3,11 @@
 require_relative '../ext/sqlite3'
 require_relative '../immutable/immutable'
 require_relative '../constants/path_constants'
+require_relative '../constants/pref_constants'
 require_relative '../sketchup_model/dictionary/speckle_model_dictionary_handler'
 
 module SpeckleConnector
   # Preferences that stored on config database and sketchup_model.
-  # rubocop:disable Metrics/ModuleLength
   module Preferences
     include Immutable::ImmutableUtils
     DICT_HANDLER = SketchupModel::Dictionary::SpeckleModelDictionaryHandler
@@ -15,91 +15,16 @@ module SpeckleConnector
     DEFAULT_PREFERENCES = '{"dark_theme":false, "diffing":false}'
 
     # @param sketchup_model [Sketchup::Model] active model.
-    # rubocop:disable Metrics/MethodLength
     def self.read_preferences(sketchup_model)
       db = Sqlite3::Database.new(SPECKLE_CONFIG_DB_PATH)
-      validate_preferences(db)
-
-      # Select data
-      row_data = db.exec("SELECT content FROM 'objects' WHERE hash = 'configSketchup'").first.first
-      # Parse string to hash
-      data_hash = JSON.parse(row_data).to_h
-      # Get current theme value
-      dark_theme = data_hash['dark_theme']
-      diffing = data_hash['diffing']
-      speckle_dictionary = sketchup_model.attribute_dictionary('Speckle')
-
-      if speckle_dictionary
-        Immutable::Hash.new(
-          {
-            user: {
-              dark_theme: dark_theme,
-              diffing: diffing
-            },
-            model: {
-              combine_faces_by_material: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :combine_faces_by_material,
-                'Speckle'
-              ),
-              include_entity_attributes: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :include_entity_attributes,
-                'Speckle'
-              ),
-              include_face_entity_attributes: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :include_face_entity_attributes,
-                'Speckle'
-              ),
-              include_edge_entity_attributes: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :include_edge_entity_attributes,
-                'Speckle'
-              ),
-              include_group_entity_attributes: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :include_group_entity_attributes,
-                'Speckle'
-              ),
-              include_component_entity_attributes: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :include_component_entity_attributes,
-                'Speckle'
-              ),
-              merge_coplanar_faces: DICT_HANDLER.get_attribute(
-                sketchup_model,
-                :merge_coplanar_faces,
-                'Speckle'
-              )
-            }
-          }
-        )
-      else
-        DICT_HANDLER.write_initial_model_data(sketchup_model, default_model_preferences)
-        Immutable::Hash.new(
-          {
-            user: {
-              dark_theme: dark_theme,
-              diffing: diffing
-            },
-            model: default_model_preferences
-          }
-        )
-      end
-    end
-    # rubocop:enable Metrics/MethodLength
-
-    def self.default_model_preferences
-      {
-        combine_faces_by_material: true,
-        include_entity_attributes: true,
-        include_face_entity_attributes: true,
-        include_edge_entity_attributes: true,
-        include_group_entity_attributes: true,
-        include_component_entity_attributes: true,
-        merge_coplanar_faces: true
-      }
+      user_preferences = validate_user_preferences(db)
+      model_preferences = validate_model_preferences(sketchup_model)
+      Immutable::Hash.new(
+        {
+          user: user_preferences,
+          model: model_preferences
+        }
+      )
     end
 
     # Whether row data is complete with preference or not.
@@ -115,11 +40,10 @@ module SpeckleConnector
 
     # Validates current preferences. If there are incomplete data then this method resets it with default preferences.
     # @param database [Sqlite3::Database] database for queries.
-    def self.validate_preferences(database)
+    def self.validate_user_preferences(database)
       row_data = database.exec("SELECT content FROM 'objects' WHERE hash = 'configSketchup'")
       is_config_sketchup_exist = !row_data.empty?
       is_data_complete = data_complete?(row_data)
-      # rubocop:disable Style/GuardClause
       if !is_config_sketchup_exist || !is_data_complete
         if is_config_sketchup_exist
           unless is_data_complete
@@ -131,8 +55,38 @@ module SpeckleConnector
           database.exec("INSERT INTO 'objects' VALUES #{DEFAULT_CONFIG}")
         end
       end
-      # rubocop:enable Style/GuardClause
+
+      # Select data
+      row_data = database.exec("SELECT content FROM 'objects' WHERE hash = 'configSketchup'").first.first
+      # Parse string to hash
+      data_hash = JSON.parse(row_data).to_h
+      # Get current theme value
+      dark_theme = data_hash['dark_theme']
+      diffing = data_hash['diffing']
+
+      {
+        dark_theme: dark_theme,
+        diffing: diffing
+      }.freeze
+    end
+
+    # @param sketchup_model [Sketchup::Model] sketchup model to validate model preferences
+    def self.validate_model_preferences(sketchup_model)
+      speckle_dictionary = sketchup_model.attribute_dictionary('Speckle')
+      if speckle_dictionary.nil?
+        DICT_HANDLER.write_initial_model_data(sketchup_model, DEFAULT_MODEL_PREFERENCES)
+        return DEFAULT_MODEL_PREFERENCES
+      end
+
+      DEFAULT_MODEL_PREFERENCES.collect do |pref_key, default_value|
+        pref_value = DICT_HANDLER.get_attribute(
+          sketchup_model,
+          pref_key,
+          'Speckle'
+        )
+        DICT_HANDLER.set_attribute(sketchup_model, pref_key, default_value, 'Speckle') if pref_value.nil?
+        pref_value.nil? ? [pref_key, default_value] : [pref_key, pref_value]
+      end.to_h
     end
   end
-  # rubocop:enable Metrics/ModuleLength
 end
