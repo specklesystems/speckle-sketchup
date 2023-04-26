c0acecae10ba7663656bb1e72c7834e7f70287c7
oguzhankoral <oguzhankoral@gmail.com>
2023-04-26 16:43:31 +0100

    Query methods for direct shapes

--- speckle_connector/src/speckle_objects/built_elements/revit/direct_shape.rb
+++ speckle_connector/src/speckle_objects/built_elements/revit/direct_shape.rb
@@ -4,6 +4,7 @@ require_relative '../../base'
 require_relative '../../other/render_material'
 require_relative '../../../constants/type_constants'
 require_relative '../../../sketchup_model/query/entity'
+require_relative '../../../sketchup_model/reader/speckle_entities_reader'
 
 module SpeckleConnector
   module SpeckleObjects
@@ -12,6 +13,9 @@ module SpeckleConnector
         # Direct shape definition for Revit mappings.
         class DirectShape < Base
           SPECKLE_TYPE = OBJECTS_BUILTELEMENTS_REVIT_DIRECTSHAPE
+          READER = SketchupModel::Reader
+          QUERY = SketchupModel::Query
+          DICTIONARY = SketchupModel::Dictionary
 
           def initialize(name:, category:, units:, base_geometries:, application_id: nil)
             super(
@@ -26,16 +30,36 @@ module SpeckleConnector
             self[:baseGeometries] = base_geometries
           end
 
+          # Collects direct shapes on selection as flat list.
+          def self.direct_shapes_on_selection(sketchup_model)
+            flat_selection_with_path = QUERY::Entity.flat_entities_with_path(
+              sketchup_model.selection,
+              [Sketchup::Face, Sketchup::ComponentInstance, Sketchup::Group], [sketchup_model]
+            )
+            mapped_selection = []
+            flat_selection_with_path.each do |entities|
+              entity = entities[0]
+              is_entity_mapped = READER::SpeckleEntitiesReader.mapped_with_schema?(entity)
+              if entity.respond_to?(:definition)
+                is_definition_mapped = READER::SpeckleEntitiesReader.mapped_with_schema?(entity.definition)
+                mapped_selection.append(entities) if is_entity_mapped || is_definition_mapped
+                next
+              end
+              mapped_selection.append(entities) if is_entity_mapped
+            end
+            mapped_selection
+          end
+
           def self.from_entity(entity, path, units, preferences)
-            schema = SketchupModel::Dictionary::SpeckleSchemaDictionaryHandler.attribute_dictionary(entity)
+            schema = DICTIONARY::SpeckleSchemaDictionaryHandler.attribute_dictionary(entity)
             if schema.nil? && entity.respond_to?(:definition)
-              schema = SketchupModel::Dictionary::SpeckleSchemaDictionaryHandler.attribute_dictionary(entity.definition)
+              schema = DICTIONARY::SpeckleSchemaDictionaryHandler.attribute_dictionary(entity.definition)
             end
             entities_with_path = []
             entities_with_path.append([entity] + path) if entity.is_a?(Sketchup::Face) || entity.is_a?(Sketchup::Edge)
             # Collect here flat list
             if entity.is_a?(Sketchup::ComponentInstance) || entity.is_a?(Sketchup::Group)
-              entities_with_path += SketchupModel::Query::Entity
+              entities_with_path += QUERY::Entity
                                     .flat_entities_with_path(
                                       entity.definition.entities, [Sketchup::Face], path.append(entity)
                                     )
@@ -52,17 +76,17 @@ module SpeckleConnector
             faces_with_path.each do |face_with_path|
               face = face_with_path[0]
               entity_path = face_with_path[1..-1]
-              parent_material = SketchupModel::Query::Entity.parent_material(entity_path)
+              parent_material = QUERY::Entity.parent_material(entity_path)
               mesh_group_id = Geometry::Mesh.get_mesh_group_id(face, preferences[:model], parent_material)
 
               if mesh_groups.key?(mesh_group_id)
                 mesh_group = mesh_groups[mesh_group_id]
-                mesh_group[0].face_to_mesh(face, SketchupModel::Query::Entity.global_transformation(face, entity_path))
+                mesh_group[0].face_to_mesh(face, QUERY::Entity.global_transformation(face, entity_path))
                 mesh_group[1].append(face)
               else
                 mesh = Geometry::Mesh.from_face(
                   face: face, units: units, model_preferences: preferences[:model],
-                  global_transform: SketchupModel::Query::Entity.global_transformation(face, entity_path),
+                  global_transform: QUERY::Entity.global_transformation(face, entity_path),
                   parent_material: parent_material
                 )
                 mesh_groups[mesh_group_id] = [mesh, [face]]
