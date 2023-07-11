d89b3772deb254d0a0ee6a8f25e7a26ee64b5f57
oguzhankoral <oguzhankoral@gmail.com>
2023-07-11 15:47:20 +0300

    Pass correct request id for getAccounts

--- speckle_connector/src/actions/init_local_accounts.rb
+++ speckle_connector/src/actions/init_local_accounts.rb
@@ -14,7 +14,7 @@ module SpeckleConnector
         puts 'Initialisation of Speckle accounts requested by plugin'
         accounts_data = state.speckle_state.accounts
         # state.with_add_queue('bindings.receiveResponse', accounts_data.to_json, [])
-        js_command = "bindings.receiveResponse('default_bindings1', '#{accounts_data.to_json}')"
+        js_command = "bindings.receiveResponse('#{_data[:request_id]}', '#{accounts_data.to_json}')"
         puts js_command
         state.with_add_queue_js_command('init_local_accounts', js_command)
       end
