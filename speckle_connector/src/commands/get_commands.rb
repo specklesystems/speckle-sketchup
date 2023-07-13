f0099090b4bd8c8eecc77d837ac7e82f362da56d
oguzhankoral <oguzhankoral@gmail.com>
2023-07-14 01:40:06 +0300

    Collect command names from related view for SketchupBridge

--- speckle_connector/src/commands/get_commands.rb
+++ speckle_connector/src/commands/get_commands.rb
@@ -8,12 +8,7 @@ module SpeckleConnector
     # Get commands.
     class GetCommands < Command
       def _run(_resolve_id, _data)
-        # view_to_get_commands = app.ui_controller.user_interfaces[view]
-        # commands_by_view = view_to_get_commands.commands.keys
         view_command_names = view.commands.keys
-        # commands_by_view = app.ui_controller.user_interfaces.collect do |id, view|
-        #   [id, view.commands.keys]
-        # end.to_h
         action = Actions::GetCommands.new(view_command_names)
         app.update_state!(action)
       end
