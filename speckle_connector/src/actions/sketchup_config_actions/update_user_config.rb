b920d822ef98a2ee9ac666407a655c62291e3c6a
oguzhankoral <oguzhankoral@gmail.com>
2023-07-26 04:14:08 +0300

    WIP: connector configs

---
+++ speckle_connector/src/actions/sketchup_config_actions/update_user_config.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../action'
+
+module SpeckleConnector
+  module Actions
+    # Action to get user config.
+    class UpdateUserConfig < Action
+      # @param state [States::State] the current state of the {App::SpeckleConnectorApp}
+      # @return [States::State] the new updated state object
+      def self.update_state(state, resolve_id, new_config)
+        puts new_config.values
+        js_script = "connectorConfigBinding.receiveResponse('#{resolve_id}')"
+        state.with_add_queue_js_command('updateUserConfig', js_script)
+      end
+    end
+  end
+end
