ef9a203c26f723410f7f07ff91aacd6e9e68cda9
oguzhankoral <oguzhankoral@gmail.com>
2023-07-30 00:31:52 +0300

    Get default filters

---
+++ speckle_connector/src/filters/send_filters.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module SpeckleConnector
+  module Filters
+    # Send filters for sketchup connector.
+    class SendFilters
+      # Get default send filters.
+      # @param sketchup_model [Sketchup::Model] active model.
+      def self.get_default(sketchup_model)
+        layer_tags = sketchup_model.layers.collect do |layer|
+          {
+            id: layer.persistent_id, name: layer.display_name, active: true
+          }
+        end
+        {
+          'everything': {
+            name: 'Everything', input: 'toggle', duplicable: false
+          },
+          'selection': {
+            name: 'Selection', input: 'toggle', duplicable: false
+          },
+          'tags': {
+            name: 'Tags', input: 'toggle', duplicable: false, tags: layer_tags, activeTags: layer_tags
+          },
+          'searchFilter': {
+            name: 'Search', input: 'search', duplicable: true
+          }
+        }.freeze
+      end
+    end
+  end
+end
