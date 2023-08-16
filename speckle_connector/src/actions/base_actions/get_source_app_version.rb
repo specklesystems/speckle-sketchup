437173e6a4e8afbdf193736df1149e8c912a77dd
oguzhankoral <oguzhankoral@gmail.com>
2023-08-16 21:54:22 +0300

    Get source app version via base bindings

--- speckle_connector/src/actions/base_actions/get_source_app_version.rb
+++ speckle_connector/src/actions/base_actions/get_source_app_version.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'action'
+require_relative '../action'
 
 module SpeckleConnector
   module Actions
-    # Get source app name.
-    class GetSourceAppName < Action
+    # Get source app version.
+    class GetSourceAppVersion < Action
       # @param state [States::State] the current state of the {App::SpeckleConnectorApp}
       # @return [States::State] the new updated state object
       def self.update_state(state, resolve_id)
-        js_command = "baseBinding.receiveResponse('#{resolve_id}', 'Sketchup')"
-        puts js_command
-        state.with_add_queue_js_command('getSourceAppName', js_command)
+        js_command = "baseBinding.receiveResponse('#{resolve_id}', #{SKETCHUP_VERSION})"
+        state.with_add_queue_js_command('getSourceAppVersion', js_command)
       end
     end
   end
