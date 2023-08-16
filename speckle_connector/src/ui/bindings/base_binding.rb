6494fd6b417e77c1e1a925b5d641e66f95470e9c
oguzhankoral <oguzhankoral@gmail.com>
2023-08-16 21:54:22 +0300

    Get source app version via base bindings

--- speckle_connector/src/ui/bindings/base_binding.rb
+++ speckle_connector/src/ui/bindings/base_binding.rb
@@ -3,7 +3,8 @@
 require_relative 'binding'
 require_relative '../../constants/path_constants'
 
-require_relative '../../actions/get_source_app_name'
+require_relative '../../actions/base_actions/get_source_app_name'
+require_relative '../../actions/base_actions/get_source_app_version'
 require_relative '../../actions/get_document_info'
 require_relative '../../actions/base_actions/add_model'
 require_relative '../../actions/base_actions/get_send_filters'
@@ -24,6 +25,7 @@ module SpeckleConnector
           # Since we send exact model card with updateModel, I can use directly AddModel action, it will replace
           updateModel: Commands::ActionCommand.new(@app, self, Actions::AddModel),
           getSourceApplicationName: Commands::ActionCommand.new(@app, self, Actions::GetSourceAppName),
+          getSourceApplicationVersion: Commands::ActionCommand.new(@app, self, Actions::GetSourceAppVersion),
           getDocumentInfo: Commands::ActionCommand.new(@app, self, Actions::GetDocumentInfo),
           updateSendFilter: Commands::ActionCommand.new(@app, self, Actions::UpdateSendFilter),
           getModelState: Commands::ActionCommand.new(@app, self, Actions::GetModelState),
