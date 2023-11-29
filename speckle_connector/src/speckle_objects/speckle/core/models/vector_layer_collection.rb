188794af8de68272d1563f3c8438ff07407fb3c9
oguzhankoral <oguzhankoral@gmail.com>
2023-11-29 03:48:03 +0300

    Add support for vector layer

---
+++ speckle_connector/src/speckle_objects/speckle/core/models/vector_layer_collection.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative 'collection'
+
+module SpeckleConnector
+  module SpeckleObjects
+    module Speckle
+      module Core
+        module Models
+          # VectorLayerCollection object that collect GIS vector elements under it's elements.
+          class VectorLayerCollection < Collection
+            SPECKLE_TYPE = 'Speckle.Core.Models.Collection:Objects.GIS.VectorLayer'
+
+            # @param state [States::State] state of the Speckle application.
+            def self.to_native(state, vector_layer_collection, layer_or_folder, entities, &convert_to_native)
+              elements = vector_layer_collection['elements']
+
+              elements.each do |element|
+                new_state, _converted_entities = convert_to_native.call(state, element, layer_or_folder, entities)
+                state = new_state
+              end
+
+              return state, []
+            end
+          end
+        end
+      end
+    end
+  end
+end
