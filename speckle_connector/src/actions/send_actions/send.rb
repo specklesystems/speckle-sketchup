b0e7d2ba049cf4746d16a50cb4353ad55027844a
oguzhankoral <oguzhankoral@gmail.com>
2024-05-08 01:34:07 +0300

    Add dummy dialog for test worker

--- speckle_connector/src/actions/send_actions/send.rb
+++ speckle_connector/src/actions/send_actions/send.rb
@@ -58,6 +58,39 @@ module SpeckleConnector
         js_script = "sendBinding.emit('sendViaBrowser', #{args.to_json})"
         state.with_add_queue_js_command('sendViaBrowser', js_script)
       end
+
+      def self.update_state_test(state, resolve_id, model_card_id)
+        dialog = UI::HtmlDialog.new(
+          {
+            :dialog_title => 'Dialog Example',
+            :preferences_key => 'com.sample.plugin',
+            :scrollable => true,
+            :resizable => true,
+            :width => 600,
+            :height => 400,
+            :left => 10,
+            :top => 10,
+            :min_width => 50,
+            :min_height => 50,
+            :max_width =>1000,
+            :max_height => 1000,
+            :style => UI::HtmlDialog::STYLE_DIALOG
+          })
+        html = '<div id="hi"><b>Hello world!</b></div>'
+        dialog.set_html(html)
+        dialog.show
+
+        action = Proc.new do |status|
+          js_command = "document.getElementById('hi').innerHTML = '<b>#{status}</b>'"
+          log_js_command = "console.log('test')"
+          dialog.execute_script(js_command)
+          dialog.execute_script(log_js_command)
+        end
+
+        selected_object_ids = state.sketchup_state.sketchup_model.selection.collect(&:persistent_id)
+        state.worker.add_jobs(1000.times.to_a.map { |i| Job.new(i, &action) })
+        state.worker.do_work(Time.now.to_f, &action)
+      end
     end
   end
 end
