09b2aaab892e3e593bd12d7552dd5b2d0deb06ef
oguzhankoral <oguzhankoral@gmail.com>
2024-06-26 10:35:10 +0300

    Converter v2

--- speckle_connector/src/actions/receive_bindings/after_get_objects.rb
+++ speckle_connector/src/actions/receive_bindings/after_get_objects.rb
@@ -2,6 +2,7 @@
 
 require_relative '../action'
 require_relative '../../convertors/to_native'
+require_relative '../../convertors/to_native_v2'
 
 module SpeckleConnector
   module Actions
@@ -12,8 +13,15 @@ module SpeckleConnector
       def self.update_state(state, resolve_id, model_card_id, source_application, root_obj)
         model_card = state.speckle_state.receive_cards[model_card_id]
         state.sketchup_state.sketchup_model.start_operation('Receive Speckle Objects', true)
-        converter = Converters::ToNative.new(state, model_card.model_id, model_card.project_name,
-                                             model_card.model_name, source_application, model_card_id)
+        converter = if root_obj['instanceDefinitionProxies']
+                      Converters::ToNativeV2.new(state, root_obj['@instanceDefinitionProxies'], model_card.model_id, model_card.project_name,
+                                                 model_card.model_name, source_application, model_card_id)
+                    else
+                      Converters::ToNative.new(state, model_card.model_id, model_card.project_name,
+                                               model_card.model_name, source_application, model_card_id)
+                    end
+        # converter = Converters::ToNative.new(state, model_card.model_id, model_card.project_name,
+        #                                      model_card.model_name, source_application, model_card_id)
         start_time = Time.now.to_f
         # Have side effects on the sketchup model. It effects directly on the entities by adding new objects.
         state = converter.receive_commit_object(root_obj)
