b75a9cebfd34ed275464b197955b3c12389915a9
oguzhankoral <oguzhankoral@gmail.com>
2024-08-01 00:04:49 +0300

    Get patch installer back

--- patch_version.py
+++ patch_version.py
@@ -45,7 +45,7 @@ def main():
 
     print(f"Patching version: {tag}")
     patch_connector(tag)
-    # patch_installer(tag)
+    patch_installer(tag) # remove it later!
 
 
 if __name__ == "__main__":
