bae9f304453c760f150090e4312a13f91cf02bae
Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>
2024-08-19 13:12:31 +0300

    Handle back materials (#373)

--- speckle_connector_3/src/convertors/to_native_v2.rb
+++ speckle_connector_3/src/convertors/to_native_v2.rb
@@ -404,6 +404,10 @@ module SpeckleConnector3
         converted_entities.each do |e|
           material_to_assign = find_material_from_proxies(obj['applicationId'].to_s)
           e.material = material_to_assign if material_to_assign
+          if from_sketchup && e.is_a?(Sketchup::Face)
+            back_material_to_assign = find_material_from_proxies("#{obj['applicationId'].to_s}_back")
+            e.back_material = back_material_to_assign if back_material_to_assign
+          end
         end
         faces = converted_entities.select { |e| e.is_a?(Sketchup::Face) }
         @converted_faces += faces if faces.any?
--- speckle_connector_3/src/sketchup_model/materials/material_manager.rb
+++ speckle_connector_3/src/sketchup_model/materials/material_manager.rb
@@ -24,19 +24,42 @@ module SpeckleConnector3
             entities, [Sketchup::ComponentInstance, Sketchup::Group, Sketchup::Face]
           )
           flat_entities.each do |entity|
-            next if entity.material.nil?
+            material = entity.material
+            back_material = nil
+            if entity.is_a?(Sketchup::Face)
+              back_material = entity.back_material
+            end
 
-            if render_material_proxies.has_key?(entity.material.persistent_id.to_s)
-              render_material_proxies[entity.material.persistent_id.to_s].add_object_id(entity.persistent_id.to_s)
-            else
-              speckle_material = SpeckleObjects::Other::RenderMaterial.from_material(entity.material)
-              render_material_proxies[entity.material.persistent_id.to_s] = SpeckleObjects::RenderMaterialProxy.new(
-                entity.material, speckle_material,
-                [entity.persistent_id.to_s], application_id: entity.material.persistent_id.to_s
-              )
+            next if material.nil? && back_material.nil?
+
+            unless material.nil?
+              if render_material_proxies.has_key?(material.persistent_id.to_s)
+                render_material_proxies[material.persistent_id.to_s].add_object_id(entity.persistent_id.to_s)
+              else
+                convert_material_and_add_to_proxies(material, entity)
+              end
+            end
+
+            unless back_material.nil?
+              if render_material_proxies.has_key?(back_material.persistent_id.to_s)
+                render_material_proxies[back_material.persistent_id.to_s]
+                  .add_object_id("#{entity.persistent_id.to_s}_back")
+              else
+                convert_material_and_add_to_proxies(back_material, entity, true)
+              end
             end
           end
           render_material_proxies.values
+        end
+
+        # @param material [Sketchup::Material]
+        def convert_material_and_add_to_proxies(material, entity, is_back_material = false)
+          speckle_material = SpeckleObjects::Other::RenderMaterial.from_material(material)
+          entity_persistent_id = is_back_material ? "#{entity.persistent_id.to_s}_back" : entity.persistent_id.to_s
+          render_material_proxies[material.persistent_id.to_s] = SpeckleObjects::RenderMaterialProxy.new(
+            material, speckle_material,
+            [entity_persistent_id], application_id: material.persistent_id.to_s
+          )
         end
       end
     end
