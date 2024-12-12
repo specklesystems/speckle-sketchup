cac7afc6a94f5b7a0b3d7fd22cb13818efaf2c91
oguzhankoral <oguzhankoral@gmail.com>
2024-12-12 23:32:23 +0300

    measure based fix

--- speckle_connector_3/src/speckle_objects/geometry/arc.rb
+++ speckle_connector_3/src/speckle_objects/geometry/arc.rb
@@ -19,22 +19,18 @@ module SpeckleConnector3
           units = arc['units']
           origin = Point.to_native(plane['origin']['x'], plane['origin']['y'], plane['origin']['z'], units)
           start_point = Point.to_native(arc['startPoint']['x'], arc['startPoint']['y'], arc['startPoint']['z'], units)
-          end_point = Point.to_native(arc['endPoint']['x'], arc['endPoint']['y'], arc['endPoint']['z'], units)
+          # end_point = Point.to_native(arc['endPoint']['x'], arc['endPoint']['y'], arc['endPoint']['z'], units)
           normal = Vector.to_native(plane['normal']['x'], plane['normal']['y'], plane['normal']['z'], units).normalize
           x_axis = Vector.to_native(plane['xdir']['x'], plane['xdir']['y'], plane['xdir']['z'], units).normalize
           radius = Geometry.length_to_native(arc['radius'], units)
 
           start_vector = (start_point - origin).normalize
-          end_vector = (end_point - origin).normalize
-
-          x_axis = start_vector.normalize if x_axis.dot(normal).abs > 0.001
+          # end_vector = (end_point - origin).normalize
 
           start_angle = Math.atan2(start_vector.cross(normal).dot(x_axis), start_vector.dot(x_axis))
-          end_angle = Math.atan2(end_vector.cross(normal).dot(x_axis), end_vector.dot(x_axis))
-          # measure = arc['measure'] # Assuming this is in radians
-          # end_angle = start_angle + measure
-
-          end_angle += 2 * Math::PI if end_angle < start_angle
+          # end_angle = Math.atan2(end_vector.cross(normal).dot(x_axis), end_vector.dot(x_axis))
+          measure = arc['measure'] # this is in radians!
+          end_angle = start_angle + measure
 
           edges = entities.add_arc(origin, x_axis, normal, radius, start_angle, end_angle)
           edges.each { |edge| edge.layer = layer }
