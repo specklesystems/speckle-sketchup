56f2b3e9344a71f1c37298e1aff08d4298d3ec2c
oguzhankoral <oguzhankoral@gmail.com>
2024-12-13 00:19:14 +0300

    backward compatible arcs without measures

--- speckle_connector_3/src/speckle_objects/geometry/arc.rb
+++ speckle_connector_3/src/speckle_objects/geometry/arc.rb
@@ -19,19 +19,23 @@ module SpeckleConnector3
           units = arc['units']
           origin = Point.to_native(plane['origin']['x'], plane['origin']['y'], plane['origin']['z'], units)
           start_point = Point.to_native(arc['startPoint']['x'], arc['startPoint']['y'], arc['startPoint']['z'], units)
-          # end_point = Point.to_native(arc['endPoint']['x'], arc['endPoint']['y'], arc['endPoint']['z'], units)
+          end_point = Point.to_native(arc['endPoint']['x'], arc['endPoint']['y'], arc['endPoint']['z'], units)
           normal = Vector.to_native(plane['normal']['x'], plane['normal']['y'], plane['normal']['z'], units).normalize
           x_axis = Vector.to_native(plane['xdir']['x'], plane['xdir']['y'], plane['xdir']['z'], units).normalize
           radius = Geometry.length_to_native(arc['radius'], units)
 
           start_vector = (start_point - origin).normalize
-          # end_vector = (end_point - origin).normalize
+          end_vector = (end_point - origin).normalize
 
           start_angle = Math.atan2(start_vector.cross(normal).dot(x_axis), start_vector.dot(x_axis))
-          # end_angle = Math.atan2(end_vector.cross(normal).dot(x_axis), end_vector.dot(x_axis))
-          measure = arc['measure'] # this is in radians!
-          end_angle = start_angle + measure
+          end_angle = Math.atan2(end_vector.cross(normal).dot(x_axis), end_vector.dot(x_axis))
 
+          measure = arc['measure'] # this is in radians!
+          if measure # for backward compatibilty
+            end_angle = start_angle + measure
+          else
+            end_angle += 2 * Math::PI if end_angle < start_angle
+          end
           edges = entities.add_arc(origin, x_axis, normal, radius, start_angle, end_angle)
           edges.each { |edge| edge.layer = layer }
           return state, edges
