3efd7678a20989cb9f2d4927d56a9513b536da40
oguzhankoral <oguzhankoral@gmail.com>
2025-10-26 09:04:02 +0300

    revert: dui url

--- speckle_connector_3/src/ui/dui3_dialog.rb
+++ speckle_connector_3/src/ui/dui3_dialog.rb
@@ -84,8 +84,8 @@ module SpeckleConnector3
           true
         end
         # File.exist?(@htm_file) ? dialog.set_file(@htm_file) : dialog.set_url('http://localhost:9091')
-        dialog.set_url('http://localhost:8082') # uncomment this line if you want to use your local version of ui
-        # dialog.set_url('https://dui.speckle.systems/') # uncomment this line if you want to use deployed ui on netlify
+        # dialog.set_url('http://localhost:8082') # uncomment this line if you want to use your local version of ui
+        dialog.set_url('https://dui.speckle.systems/') # uncomment this line if you want to use deployed ui on netlify
         add_exec_callback(dialog)
         dialog
       end
